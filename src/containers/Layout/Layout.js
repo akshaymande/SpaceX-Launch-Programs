f5327a9c662a48fbf3fbff87252ae2fc494ce33d
akshay-mande <akshay.mande@dentsuaegis.com>
2020-11-30 10:54:00 +0530

    feat: initial commit

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Layout extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+               Test
+            </React.Fragment>
+        )
+    }
+}
+export default Layout
